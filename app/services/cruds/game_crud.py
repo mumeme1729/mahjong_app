678667c436175ca5ea6f64ea36ea29da2dbd1bfc
mumeme1729 <mumeme.exe@gmail.com>
2022-01-23 15:00:33 +0900

    対局テーブルの変更機能を追加

--- app/services/cruds/game_crud.py
+++ app/services/cruds/game_crud.py
@@ -4,6 +4,7 @@ from uuid import UUID
 from venv import create
 from sqlalchemy.orm.session import Session
 from datetime import datetime, timedelta
+from schemas.game import GameUpdata
 from models.profiles import ProfileTable
 from models.games import GamesTable
 from sqlalchemy.orm import joinedload
@@ -30,7 +31,37 @@ def set_game(game_data:GameCreate,profile:ProfileTable,db:Session)->dict:
     db.commit()
     return game.id
 
+def update_game_and_result(game_data:GameUpdata,profile:ProfileTable,db:Session):
+    """
+    ゲームテーブルを更新する
+    """
+    #対象のゲームを取得
+    game = get_game_by_id(game_data.id,db)
+
+    dt = datetime.now()
+    dt.strftime("%Y/%m/%d %H:%M:%S")
+    game.update_at = dt
+    game.updater = profile.id
+
+    # アップデートする情報
+    for update_result in game_data.game_results:
+        # アップデート前の情報
+        for pre_result in game.game_results:
+            # プロフィールが一致した場合更新する
+            if(pre_result.profile == update_result.profile):
+                pre_result.rank = update_result.rank
+                pre_result.score = update_result.score
+                pre_result.update_at = dt
+
+    db.commit()
+    return game.id       
+
+
+
 def get_game_by_id(game_id:UUID,db:Session)->GamesTable:
+    """
+    対象のゲームをidから検索
+    """
     return db.query(GamesTable).options(joinedload(GamesTable.game_results)).filter(GamesTable.id == game_id).first()
 
 ### DELETE ###
--- app/tests/test_group.py
+++ app/tests/test_group.py
@@ -1,9 +1,12 @@
+from ast import Param
+from datetime import timedelta
+import uuid
+from services.authenticates.create_access_token import create_access_token
 from main import app
 from starlette.testclient import TestClient
 
 client = TestClient(app)
 
-token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QGV4YW1wbGUuY29tIiwiZXhwIjoxNjQzMDI4NDczfQ.Rbj5sSvkqMh9dP3u_Bfm_Yr593sYeoq-i5fklu2Hrg8"
 
 def test_create_group_without_token():
     """
@@ -43,6 +46,10 @@ def test_create_group_with_correct_token():
     """
     グループ作成 成功
     """
+    access_token_expires = timedelta(minutes = 1800)
+    token = create_access_token(
+        token_payload={"sub": "testuser@example.com"},expires_delta=access_token_expires
+    )
     auth = f"Bearer {token}"
     response = client.post(
         "/api/groups/create_groups", 
@@ -56,13 +63,39 @@ def test_create_group_with_correct_token():
     )
     assert response.status_code == 200 or response.status_code == 400
 
-def test_join_group():
+# def test_join_group():
+#     """
+#     グループ参加 成功 or 既に参加している
+#     """
+#     #トークン取得
+#     access_token_expires = timedelta(minutes = 1800)
+#     token = create_access_token(
+#         token_payload={"sub": "testuser6@example.com"},expires_delta=access_token_expires
+#     )
+#     auth = f"Bearer {token}"
+#     response = client.put(
+#         "/api/groups/join_group", 
+#         headers={"Authorization": auth},
+#         json={
+#             "title": "string",
+#             "password": "string",
+#             "text": "string",
+#             "image": "string"
+#         }
+#     )
+#     assert response.status_code == 200 or (response.status_code == 400 and response.detail == "already joined this group")
+
+def test_leave_group_success():
     """
-    グループ参加 成功 or 既に参加している
+    グループ脱退成功パターン
     """
-    #トークン取得
+    access_token_expires = timedelta(minutes = 1800)
+    token = create_access_token(
+        token_payload={"sub": "testuser@example.com"},expires_delta=access_token_expires
+    )
     auth = f"Bearer {token}"
-    response = client.post(
+    #グループを作成する
+    res = client.post(
         "/api/groups/create_groups", 
         headers={"Authorization": auth},
         json={
@@ -72,4 +105,79 @@ def test_join_group():
             "image": "string"
         }
     )
-    assert response.status_code == 200 or (response.status_code == 400 and response.detail == "already joined this group")
+    assert res.status_code == 200
+    
+    response = client.put(
+        "/api/groups/leave_group", 
+        headers={"Authorization": auth},
+        params={
+            "group_id": uuid.UUID(res.text[1:37]),
+        }
+    )
+
+    assert response.status_code == 200
+
+def test_leave_group_dont_belong_group():
+    """
+    グループ脱退失敗パターン(グループに属していない)
+    """
+    access_token_expires = timedelta(minutes = 1800)
+    token = create_access_token(
+        token_payload={"sub": "testuser@example.com"},expires_delta=access_token_expires
+    )
+    auth = f"Bearer {token}"
+   
+    response = client.put(
+        "/api/groups/leave_group", 
+        headers={"Authorization": auth},
+        params={
+            "group_id": uuid.UUID("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
+        }
+    )
+
+    assert response.status_code == 400
+
+
+def test_leave_group_dont_belong_group():
+    """
+    グループ脱退失敗パターン(既に非アクティブ)
+    """
+    access_token_expires = timedelta(minutes = 1800)
+    token = create_access_token(
+        token_payload={"sub": "testuser@example.com"},expires_delta=access_token_expires
+    )
+    auth = f"Bearer {token}"
+    #グループを作成する
+    res = client.post(
+        "/api/groups/create_groups", 
+        headers={"Authorization": auth},
+        json={
+            "title": "string",
+            "password": "string",
+            "text": "string",
+            "image": "string"
+        }
+    )
+    assert res.status_code == 200
+    
+    res1 = client.put(
+        "/api/groups/leave_group", 
+        headers={"Authorization": auth},
+        params={
+            "group_id": uuid.UUID(res.text[1:37]),
+        }
+    )
+
+    assert res1.status_code == 200
+
+
+    response = client.put(
+        "/api/groups/leave_group", 
+        headers={"Authorization": auth},
+        params={
+            "group_id": uuid.UUID(res.text[1:37]),
+        }
+    )
+
+
+    assert response.status_code == 400
